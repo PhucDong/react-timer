b257a2c377079dbde5cd1a35e784fed8e4d67a96
Phuc <dongvuminhphuc@gmail.com>
2023-07-19 17:03:46 +0700

    Fix the reset feature of the timer

--- src/useTimer.js
+++ src/useTimer.js
@@ -7,9 +7,7 @@ const useTimer = (ini = 0) => {
     seconds: 0,
   });
 
-  // const isStart = useRef();
   const active = useRef();
-  console.log(12, active);
   const refInterval = useRef();
 
   const startTimer = () => {
@@ -18,12 +16,12 @@ const useTimer = (ini = 0) => {
 
     refInterval.current = setInterval(function () {
       const nowTime = Math.floor(Date.now() / 1000); // time when the clock ticks
-      console.log(20, startTime, nowTime);
+
       let diffTime = nowTime - startTime;
       let hours = Math.floor(diffTime / 3600);
       let minutes = Math.floor((diffTime - hours * 3600) / 60);
       let seconds = diffTime - hours * 3600 - minutes * 60;
-      // console.log(hours, minutes, seconds);
+
       setTime({ ...time, hours: hours, minutes: minutes, seconds: seconds });
     }, 1000);
   };
@@ -35,7 +33,7 @@ const useTimer = (ini = 0) => {
 
   const resetTimer = () => {
     setTime({ ...time, hours: 0, minutes: 0, seconds: 0 });
-    refInterval.current = null;
+    clearInterval(refInterval.current);
     active.current.disabled = false;
   };
 
